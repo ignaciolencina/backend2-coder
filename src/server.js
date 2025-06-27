d1a55741e59e5fe952af6651f92b24944b2e8c97
Ignacio Lencina <nacholencina60@gmail.com>
2025-06-26 21:24:47 -0300

    Conectar a la db de mongo y al servidor

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import morgan from 'morgan';
+
+import './database/database.js';
+
+import { mainRouter } from './routes/mainRouter.js';
+
+console.clear();
+console.log('⌛ Inicializando servidor...');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use('/api/v1', mainRouter);
+
+app.listen(PORT, () => {
+  console.log(`🥟 El servidor está arriba y en el puerto ${PORT}`);
+});
