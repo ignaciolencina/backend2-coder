bb8d3b5ac4c3f481d8d540fa9f4fd86ad08537a1
Ignacio Lencina <nacholencina60@gmail.com>
2025-08-10 21:26:37 -0300

    Roles en manejo de products

--- src/routes/routers/productRouter.js
+++ src/routes/routers/productRouter.js
@@ -1,26 +1,46 @@
 import express from 'express';
+import passport from 'passport';
 
 import { Products } from '../../controllers/products/productsIndex.js';
-import { post_productValidationSchema, put_productValidationSchema } from '../../helpers/validations/productValidationSchema.js';
+import {
+  post_productValidationSchema,
+  put_productValidationSchema,
+} from '../../helpers/validations/productValidationSchema.js';
 import { validateBody } from '../../middlewares/validateBody.js';
+import { requireRole } from '../../middlewares/auth.js';
 
 export const productRouter = express.Router();
 
+productRouter.use(passport.authenticate('current', { session: false }));
+
 productRouter.post(
   '/',
+  requireRole(['admin']),
   (req, res, next) =>
     validateBody(req, res, next, post_productValidationSchema),
   Products.PostController.postProduct,
 );
 
-productRouter.get('/', Products.GetController.getProducts);
-productRouter.get('/:id', Products.GetController.getProduct);
+productRouter.get(
+  '/',
+  requireRole(['user', 'admin']),
+  Products.GetController.getProducts,
+);
+productRouter.get(
+  '/:id',
+  requireRole(['user', 'admin']),
+  Products.GetController.getProduct,
+);
 
 productRouter.put(
   '/:id',
-  (req, res, next) =>
-    validateBody(req, res, next, put_productValidationSchema),
+  requireRole(['admin']),
+  (req, res, next) => validateBody(req, res, next, put_productValidationSchema),
   Products.PutController.putProduct,
 );
 
-productRouter.delete('/:id', Products.DeleteController.deleteProduct);
+productRouter.delete(
+  '/:id',
+  requireRole(['admin']),
+  Products.DeleteController.deleteProduct,
+);
